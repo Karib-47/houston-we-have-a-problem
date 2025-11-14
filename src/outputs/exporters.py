ae2c501a87fa6365565dc7b3dff0f0b090fa9532
Karib-47 <mdkaribislam47@gmail.com>
2025-11-14 10:19:25 +0500

    feat: add src/outputs/exporters.py

---
+++ src/outputs/exporters.py
@@ -0,0 +1,7 @@
+thonimport json
+from pathlib import Path
+
+def export_json(data, output_path: Path):
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
